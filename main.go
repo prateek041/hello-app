efeec8f3f3a829831ed3c770d80a38cfeb8276a9
prateek041 <prateeksingh9741@gmail.com>
2023-04-27 15:23:41 +0530

    added one more route
    Signed-off-by: prateek041 <prateeksingh9741@gmail.com>

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func mainHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "Hello world this is testing")
 }
 
+func secondaryHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, "This is the secondary route")
+}
+
 func main() {
 	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/second", secondaryHandler)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8081", nil)
 
 	if err != nil {
 		log.Fatal("Error in starting the server", err)
